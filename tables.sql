64a5027a7408d86acfebf7d97d69d2cc10809456
SwG Ghosh <codecrafts.cf@icloud.com>
2018-06-17 14:38:00 +0530

    Tables.sql updated for new data type of lat, long

--- tables.sql
+++ tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE signals (
     id SERIAL PRIMARY KEY,
     signalid TEXT NOT NULL,
-    latitude REAL,
-    longitude REAL
+    latitude DECIMAL PRECISION,
+    longitude DECIMAL PRECISION
 );
 
 CREATE TABLE devicelocations (
@@ -12,6 +12,6 @@ CREATE TABLE devicelocations (
     trackname TEXT,
     phone BIGINT,
     trainno INT,
-    latitude REAL,
-    longitude REAL
+    latitude DECIMAL PRECISION,
+    longitude DECIMAL PRECISION
 );
